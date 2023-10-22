e2ea028ebd188a55d3d1cf4c0f16c65b778d5b59
Manuel De Leon <manuel@deleonnet.com>
2023-10-21 23:50:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'transportador-william',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
